e728fbee31db516f8a3585621d40d00e042f351e
mramos <mramos@virtela.net>
2018-08-27 02:50:44 +0800

    Added additional user

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -1,6 +1,8 @@
 INSERT INTO users(id, username, password, enabled) VALUES(1, 'max.a.ramos@gmail.com', '$2a$10$wTrJXHGYS31v1jSVUdACoelFSHA.g13eBA.wjQnkb56rzAJPUlW8y', TRUE);
+INSERT INTO users(id, username, password, enabled) VALUES(2, 'max_a_ramos@yahoo.com', '$2a$10$wTrJXHGYS31v1jSVUdACoelFSHA.g13eBA.wjQnkb56rzAJPUlW8y', TRUE);
 
 INSERT INTO authorities(id, authority) VALUES(1, 'ROLE_ADMIN');
 INSERT INTO authorities(id, authority) VALUES(2, 'ROLE_USER');
 
 INSERT INTO users_authorities(user_id, authority_id) VALUES(1, 1);
+INSERT INTO users_authorities(user_id, authority_id) VALUES(2, 2);
