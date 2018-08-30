fc6a3fca6232aeff3d21e8e8d6fed4bfe151bb41
mramos <mramos@virtela.net>
2018-08-31 04:47:14 +0800

    Added admin home

--- src/main/java/com/maxaramos/springdatajpatest/controller/AdminController.java
+++ src/main/java/com/maxaramos/springdatajpatest/controller/AdminController.java
@@ -1,8 +1,25 @@
 package com.maxaramos.springdatajpatest.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.maxaramos.springdatajpatest.model.User;
+import com.maxaramos.springdatajpatest.service.UserService;
 
 @Controller
 public class AdminController {
+
+	@Autowired
+	private UserService userService;
+
+	@GetMapping("/admin/home")
+	public String home(Model model, Authentication authentication) {
+		User user = userService.findByUsername(authentication.getName());
+		model.addAttribute("user", user);
+		return "/admin/home";
+	}
 
 }
